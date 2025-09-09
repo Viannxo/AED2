a4498908e960773e4172ba012d2b376c614413d8
Viannxo <eduardo.vmeira@gmail.com>
2025-09-08 22:10:12 -0300

    gotta finish

---
+++ TPs/TP3/inverString.java
@@ -0,0 +1,17 @@
+package TPs.TP3;
+
+import java.util.Scanner;
+
+public class inverString {
+    public static boolean Parada(String line) {
+        return (line.length() == 3 &&
+                line.charAt(0) == 'F' &&
+                line.charAt(1) == 'I' &&
+                line.charAt(2) == 'M');
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
