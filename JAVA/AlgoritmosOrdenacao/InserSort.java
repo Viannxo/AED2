0310cb21dfa70017099371c9494ee3b5da753cd3
Viannxo <eduardo.vmeira@gmail.com>
2025-09-09 14:58:45 -0300

    add

--- JAVA/AlgoritmosOrdenacao/InserSort.java
+++ JAVA/AlgoritmosOrdenacao/InserSort.java
@@ -1,5 +1,52 @@
 package JAVA.AlgoritmosOrdenacao;
 
+import java.util.Scanner;
+
+    // [3, 1 , 8 , 6 , 2]
+    // [1, 3, 8 , 6 , 2 ]
+    // [1, 3, 6 , 8 , 2 ]
+    // [1, 2, 3 , 6 , 8 ]
+
 public class InserSort {
-    
+
+    Scanner sc = new Scanner(System.in);
+    int n;
+    int[] vet;
+    int j;
+
+    public int[] input(int n) {
+        this.n = n;
+        vet = new int[n];
+        for (int i = 0; i < n; i++) {
+            vet[i] = sc.nextInt();
+        }
+        return vet;
+    }
+
+    private static void InsertionSort(int[] vet, int n) {
+        for (int i = 1; i < n; i++) {
+            int aux=vet[i];
+            int j=i-1;
+            while(j>=0 && vet[j]>aux){
+                vet[j+1]=vet[j];
+                j--;
+            }
+            vet[j+1]=aux;
+        }
+    }
+
+    public void print(int[] vet, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(vet[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        InserSort is = new InserSort();
+        is.n = is.sc.nextInt();
+        is.vet = is.input(is.n);
+        InsertionSort(is.vet, is.n);
+        is.print(is.vet, is.n);
+        is.sc.close();
+        
+    }
 }
--- JAVA/Lab/gridLargada.java
+++ JAVA/Lab/gridLargada.java
@@ -1,5 +1,34 @@
 package JAVA.Lab;
 
+import java.util.Scanner;
+
 public class gridLargada {
-    
+    int n; //numero de carros
+    int[] vet; //vetor posições
+    int cont; //contador de movimentos
+
+    Scanner sc = new Scanner(System.in);
+
+    public int[] input (int n){
+        this.n = n;
+        vet = new int[n];
+        for (int i = 0; i < n; i++) {
+            vet[i] = i + 1;
+        }
+        return vet;
+    }
+
+    public static void main(String[] args) {
+        gridLargada gl = new gridLargada();
+        gl.n = gl.sc.nextInt();
+        System.out.println(gl.n);
+        gl.vet = gl.input(gl.n);
+        for (int i = 0; i < gl.n; i++) {
+            System.out.print(gl.vet[i] + " ");
+        }
+        gl.sc.close();
+    }
+
 }
+
+//movimentação pra esquerda é positiva e movimentação pra direita é nula
